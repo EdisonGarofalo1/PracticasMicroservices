ff36b414f464c11b66d9a28f131f640042b5686c
EdisonGarofalo1 <edisongarofalo88@gmail.com>
2024-03-21 17:01:29 -0500

    config server

--- categoria-service/src/main/java/com/aplicativo/categoria/controllers/CategoriaController.java
+++ categoria-service/src/main/java/com/aplicativo/categoria/controllers/CategoriaController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aplicativo.categoria.CategoriaServiceApplication;
 import com.aplicativo.categoria.models.entity.Categoria;
 import com.aplicativo.categoria.services.CategoriaService;
 
--- iventario-service/src/main/java/com/aplicativo/iventario/controllers/IventarioController.java
+++ iventario-service/src/main/java/com/aplicativo/iventario/controllers/IventarioController.java
@@ -1,5 +1,5 @@
 package com.aplicativo.iventario.controllers;
-
+import org.springframework.http.HttpStatus;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.http.HttpStatus;
 import com.aplicativo.iventario.models.entity.Inventario;
 import com.aplicativo.iventario.services.InventarioService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/Iventario")
@@ -23,18 +23,18 @@ public class IventarioController {
 	
 	
 	@Autowired
-	private InventarioService  InventarioService;
+	private InventarioService  inventarioService;
 	
 	@GetMapping("/listar")
 
 	public List<Inventario> listar() {
-		return InventarioService.findAll();
+		return inventarioService.findAll();
 	}
 	
 	@GetMapping("/ver/{id}")
 
 	public Inventario detalle(@PathVariable Long id) throws Exception {
-		return InventarioService.findById(id);
+		return inventarioService.findById(id);
 	}
 	
 	
@@ -42,7 +42,7 @@ public class IventarioController {
 
 	@ResponseStatus(HttpStatus.CREATED)
 	public Inventario crear(@RequestBody Inventario inventario) throws Exception {
-		return InventarioService.save(inventario);
+		return inventarioService.save(inventario);
 	}
 	
 	@PutMapping("/editar/{id}")
@@ -51,10 +51,10 @@ public class IventarioController {
 	public Inventario editar(@RequestBody Inventario inventario, @PathVariable Long id) throws Exception {
 
 		try {
-			Inventario invetarioDB = InventarioService.findById(id);
+			Inventario invetarioDB = inventarioService.findById(id);
 			invetarioDB.setNombre(inventario.getNombre());
 			invetarioDB.setCantidad(inventario.getCantidad());
-			return InventarioService.save(invetarioDB);
+			return inventarioService.save(invetarioDB);
 		} catch (Exception e) {
 			throw new Exception(e.getMessage());
 		}
